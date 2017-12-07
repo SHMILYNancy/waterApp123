a19ed737e48fd77e6467c32c14ba5096e384539d
yt0379 <yt0379@qq.com>
2017-12-07 18:18:37 +0800

    隐藏tabs相关代码方式更改

--- www/js/app.js
+++ www/js/app.js
@@ -27,17 +27,22 @@ angular.module('starter', ['ionic','starter.controllers', 'starter.services'])
 })
 
 
-  .directive('hideTabs',function($rootScope){
-    return {
-      restrict: 'AE',
-      link: function ($scope) {
-        $rootScope.hideTabs = 'tabs-item-hide';
-        $scope.$on('$destroy',function(){
-          $rootScope.hideTabs = ' ';
-        })
-      }
+.directive('hideTabs', function($rootScope) {
+  return {
+    restrict: 'A',
+    link: function(scope, element, attributes) {
+      scope.$on('$ionicView.beforeEnter', function() {
+        scope.$watch(attributes.hideTabs, function(value){
+          $rootScope.hideTabs = value;
+        });
+      });
+
+      scope.$on('$ionicView.beforeLeave', function() {
+        $rootScope.hideTabs = false;
+      });
     }
-  })
+  };
+})
 
 
 
