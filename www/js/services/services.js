e3d9ed3b7c398a9ddd5271cf475e411e05c2758e
Taneeya <2240351540@qq.com>
2017-12-08 11:02:40 +0800

    我的->积分

---
+++ www/js/services/services.js
@@ -0,0 +1,63 @@
+angular.module('starter.services', ['address.services'])
+
+  .factory('Address',function ($http) {
+    return {
+      submit:function (data) {
+        return $http({
+          method:"POST",
+          url:"http://10.0.29.80/demo/waterApp/www/php/index.php?a=Address&b=submit",
+          data:data,
+          headers:{'Content-Type': 'application/x-www-form-urlencoded'}
+        })
+      }
+    }
+  })
+
+.factory('Chats', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var chats = [{
+    id: 0,
+    name: 'Ben Sparrow',
+    lastText: 'You on your way?',
+    face: 'img/ben.png'
+  }, {
+    id: 1,
+    name: 'Max Lynx',
+    lastText: 'Hey, it\'s me',
+    face: 'img/max.png'
+  }, {
+    id: 2,
+    name: 'Adam Bradleyson',
+    lastText: 'I should buy a boat',
+    face: 'img/adam.jpg'
+  }, {
+    id: 3,
+    name: 'Perry Governor',
+    lastText: 'Look at my mukluks!',
+    face: 'img/perry.png'
+  }, {
+    id: 4,
+    name: 'Mike Harrington',
+    lastText: 'This is wicked good ice cream.',
+    face: 'img/mike.png'
+  }];
+
+  return {
+    all: function() {
+      return chats;
+    },
+    remove: function(chat) {
+      chats.splice(chats.indexOf(chat), 1);
+    },
+    get: function(chatId) {
+      for (var i = 0; i < chats.length; i++) {
+        if (chats[i].id === parseInt(chatId)) {
+          return chats[i];
+        }
+      }
+      return null;
+    }
+  };
+});
